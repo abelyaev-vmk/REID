f70b157a4545f0af717e463f594697308d3029c1
Andrew Belyaev <abelyaev.vmk@gmail.com>
2017-04-14 00:30:15 +0300

    Visualize

--- tools/plot_train_curve.py
+++ tools/plot_train_curve.py
@@ -24,7 +24,7 @@ def parse_args():
     return args
 
 
-def parse_train_log(log_path):
+def parse_global_train_loss(log_path):
     iters = []
     losses = []
 
@@ -39,13 +39,42 @@ def parse_train_log(log_path):
     return np.array(iters[5:]), np.array(losses[5:])
 
 
+def parse_bbox_train_loss(log_path):
+    losses = []
+
+    with open(log_path, 'r') as f:
+        for line in f.readlines():
+            m = re.match(r".*loss_bbox = ([-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)*", line)
+            if m:
+                losses.append(float(m.group(0)))
+
+    return np.array(losses[5:])
+
+def parse_bbox_pid_loss(log_path):
+    iters = []
+    losses = []
+
+    with open(log_path, 'r') as f:
+        for line in f.readlines():
+            m = re.match(r".*Iteration ([-+]?\d+), loss = ([-+]?(\d+(\.\d*)?|\.\d+)([eE][-+]?\d+)?)", line)
+            if m:
+                iter, loss = m.group(1, 2)
+                iters.append(int(iter))
+
+    return np.array(iters[5:]), np.array(losses[5:])
+
+
 def moving_average(a, n=3):
-    ret = np.cumsum(a, dtype=float)
-    ret[n:] = ret[n:] - ret[:-n]
-    print(a.shape)
-    ret[:n - 1] /= np.arange(1, n)
-    ret[n - 1:] /= n
-    return ret
+    try:
+        ret = np.cumsum(a, dtype=float)
+        ret[n:] = ret[n:] - ret[:-n]
+        print(a.shape)
+        ret[:n - 1] /= np.arange(1, n)
+        ret[n - 1:] /= n
+        return ret
+    except ValueError as e:
+        print('Now n=%d because of error' % (n-1), e)
+        return moving_average(a, n - 1) if n > 2 else 0
 
 
 if __name__ == '__main__':
@@ -53,7 +82,8 @@ if __name__ == '__main__':
 
     log_path = args.log_path
 
-    iters, losses = parse_train_log(log_path)
+    iters, global_losses = parse_global_train_loss(log_path)
+    bbox_losses = parse_bbox_train_loss(log_path)
 
     from bokeh.plotting import figure
     from bokeh.embed import file_html
@@ -63,11 +93,12 @@ if __name__ == '__main__':
 
     p1 = figure(width=1600, height=800, title="Train loss from %s" % log_path,
                 tools=TOOLS, webgl=True)
-    p1.line(iters, losses, legend="Train loss")
+    p1.line(iters, global_losses, legend="Train loss")
+    p1.line(iters, bbox_losses, legend="BBox loss")
     p1.ygrid[0].ticker.desired_num_ticks = 30
     p1.xgrid[0].ticker.desired_num_ticks = 20
 
-    p1.line(iters, moving_average(losses, 30),
+    p1.line(iters, moving_average(global_losses, 100),
             color='red', legend="MA100 Train loss")
 
     html = file_html(p1, CDN, title=log_path)
