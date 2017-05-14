298381444abcffe6869321e29151963518adc6f7
Andrew Belyaev <abelyaev.vmk@gmail.com>
2017-05-14 15:40:32 +0300

    Testing layers

--- lib/layers/DataLayer.py
+++ lib/layers/DataLayer.py
@@ -53,5 +53,5 @@ class TestDataLayer(caffe.Layer):
         img = np.array(img, dtype=np.float32)
         im_scale = 244
         img = cv.resize(img, (244, 244), interpolation=cv.INTER_LINEAR)
-        img = img.transpose((2, 0, 1))
+        img = img.transpose((2, 1, 0))
         return img, [x1, y1, x2, y2]
