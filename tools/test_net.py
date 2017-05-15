49f8ccac4c9b9ee7e5ef420c088177f9d145dd39
Andrew Belyaev <abelyaev.vmk@gmail.com>
2017-05-15 13:35:51 +0300

    Testing layers

--- tools/test_net.py
+++ tools/test_net.py
@@ -66,7 +66,79 @@ def load_probe(rois_dir, images_dir):
 
 
 def evaluate(log_dir):
-    pass
+    import pickle
+    import json
+    import numpy as np
+    from sklearn.metrics import average_precision_score
+
+    def _compute_iou(a, b):
+        x1 = max(a[0], b[0])
+        y1 = max(a[1], b[1])
+        x2 = min(a[2], b[2])
+        y2 = min(a[3], b[3])
+        inter = max(0, x2 - x1) * max(0, y2 - y1)
+        union = (a[2] - a[0]) * (a[3] - a[1]) + (b[2] - b[0]) * (b[3] - b[1]) - inter
+        return inter * 1.0 / union
+
+    gallery_feat = json.load(open('gallery_features.pkl', 'r'))
+    gallery_det = json.load(open('videoset.json', 'r'))
+    probe_feat = np.load(open('probe_features.npy', 'rb'))
+    gt_marking = json.load(open(osp.join(cfg.TEST.DATASETS[0].PATH[:-5], 'marking.json'), 'r'))
+    name_to_det_feat = {}
+    for name, feat in gallery_feat:
+        inds = np.where(np.array(list(map(lambda gd: gd['score'], gallery_det[name]))) >= 0.5)[0]
+        if len(inds) > 0:
+            det = np.array(gallery_det[name])[inds]
+            det = np.array(list(map(lambda d: [d['x'], d['y'], d['w'], d['h']], det)))
+            feat = np.array(feat)[inds]
+            name_to_det_feat[name] = (det, feat)
+    num_images = sum([len(gt) for _, gt in gallery_det.items()])
+    num_probe = len(probe_feat)
+
+    aps = []
+    top1_acc = []
+    for i in range(num_probe):
+        y_true, y_score = [], []
+        feat_p = probe_feat[i][1][np.newaxis, :]
+        imname = probe_feat[i][0].split('/')[-1]
+        count_gt = 0
+        count_tp = 0
+        for item in gt_marking[imname]:
+            gt = np.array([item['x'], item['y'], item['w'], item['h']])
+            count_gt += (gt[2] * gt[3] > 0)
+            if imname not in name_to_det_feat:
+                continue
+            det, feat_g = name_to_det_feat[imname]
+            dis = np.sum((feat_p - feat_g) ** 2, axis=1)
+            label = np.zeros(len(dis), dtype=np.int32)
+            if gt[2] * gt[3] > 0:
+                w, h = gt[2], gt[3]
+                gt[2:] += gt[:2]
+                thresh = min(0.5, (w * h * 1.0) / ((w + 10) * (h + 10)))
+                inds = np.argsort(dis)
+                dis = dis[inds]
+                # set the label of the first box matched to gt to 1
+                for j, roi in enumerate(det[inds, :4]):
+                    roi[2:] += roi[:2]
+                    if _compute_iou(roi, gt) >= thresh:
+                        label[j] = 1
+                        count_tp += 1
+                        break
+            y_true.extend(list(label))
+            y_score.extend(list(-dis))
+
+        assert count_tp <= count_gt
+        recall_rate = count_tp * 1.0 / count_gt
+        ap = average_precision_score(y_true, y_score) * recall_rate
+        if not np.isnan(ap):
+            aps.append(ap)
+        else:
+            aps.append(0)
+        maxind = np.argmax(y_score)
+        top1_acc.append(y_true[maxind])
+
+    print('mAP: {:.2%}'.format(np.mean(aps)))
+    print('top-1: {:.2%}'.format(np.mean(top1_acc)))
 
 
 if __name__ == '__main__':
